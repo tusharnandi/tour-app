706a0562b36cafb8945c9ea7b803eac57f0277b4
Tushar Kanti Nandi <tusharnandi@gmail.com>
2023-01-07 15:41:15 +0530

    Step-5 Filtering Repeaters and e2e testing

--- e2e-tests/scenarios.js
+++ e2e-tests/scenarios.js
@@ -5,8 +5,32 @@
 
 describe('Tour App', function() {
 
-  beforeEach(function() {
-    browser.get('index.html');
-  });
+    describe('photoList', function () {
 
+        //////////
+        beforeEach(function () {
+            browser.get('index.html');
+        });
+
+        it('should filter the photo list as a user types into the search box', function () {
+
+            var photoList = element.all(by.repeater('photo in $ctrl.photos'));
+
+            var query = element(by.model('$ctrl.query'));
+
+            expect(photoList.count()).toBe(3);
+
+            query.sendKeys('nexus');
+            expect(photoList.count()).toBe(1);
+
+            query.clear();
+            query.sendKeys('motorola');
+            expect(photoList.count()).toBe(2);
+
+
+        });
+
+        //////////
+    });
+  
 });
